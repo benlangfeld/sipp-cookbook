18811d5795063c5a46c16d8e910e90d9f84f8e74
Ben Langfeld <ben@langfeld.me>
2013-10-16 15:57:43 -0300

    Proper idempotence

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,7 @@ version = node['sipp']['version']
 remote_file "#{Chef::Config[:file_cache_path]}/sipp-#{version}.tar.gz" do
   source "http://sourceforge.net/projects/sipp/files/sipp/#{version}/sipp-#{version}.tar.gz"
   checksum "8c1d513423f9dabee799e738b737e311"
+  action :create_if_missing
 end
 
 cookbook_file "#{Chef::Config[:file_cache_path]}/sipp_dyn_pcap.diff" do
@@ -21,8 +22,5 @@ script "compile sipp" do
   make pcapplay
   cp sipp /usr/local/bin
 EOF
-  not_if do
-    system 'sipp'
-    $?.exitstatus == 99
-  end
+  not_if 'test -f /usr/local/bin/sipp'
 end
