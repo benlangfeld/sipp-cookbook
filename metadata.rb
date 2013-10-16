aa3778b8ab16f93746c9885a74579a4cd0d6cd2c
Ben Langfeld <ben@langfeld.me>
2013-10-16 17:21:24 -0300

    Attributes in metadata break everything
    See https://github.com/RiotGames/vagrant-berkshelf/issues/53

--- metadata.rb
+++ metadata.rb
@@ -10,18 +10,3 @@ recipe            "sipp", "Fetches, patches, and installs SIPp"
 %w{ ubuntu debian }.each do |os|
   supports os
 end
-
-attribute 'sipp',
-  :display_name => 'SIPp',
-  :description  => 'Hash of SIPp attributes',
-  :type         => 'String'
-
-attribute 'sipp/version',
-  :display_name => 'SIPp Version',
-  :description  => 'The version of SIPp to install',
-  :type         => 'String'
-
-attribute 'sipp/build_dependencies',
-  :display_name => 'SIPp Build Dependencies',
-  :description  => 'The OS packages to install as dependencies for building SIPp',
-  :type         => 'Array'
